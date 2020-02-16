ee4158e5297b06d46d836cdbdec52d594d09486e
Edgar Berber <e_berber@u.pacific.edu>
2020-02-15 20:11:55 -0800

    text to text API enabled

---
+++ translator.py
@@ -0,0 +1,27 @@
+import argparse
+import six
+
+def translate_text():
+    """Translates text into the target language.
+    Target must be an ISO 639-1 language code.
+    """
+    target = 'en'
+    text = 'Como te llamas?'
+    from google.cloud import translate_v2 as translate
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(
+        text, target_language=target)
+
+    print(u'Text: {}'.format(result['input']))
+    print(u'Translation: {}'.format(result['translatedText']))
+    print(u'Detected source language: {}'.format(
+        result['detectedSourceLanguage']))
+    return result['translatedText']
+
+translated_text = translate_text()
