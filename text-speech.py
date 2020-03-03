749c4437d2e4c2bbac5c6fa38a25e638032d9c9e
kbamnolk <k_bamnolker@u.pacific.edu>
2020-03-02 21:27:20 -0800

    text to speech script working using google cloud api, file is mp3 and hardcoded text for now

--- text-speech.py
+++ text-speech.py
@@ -5,7 +5,7 @@ from google.cloud import texttospeech
 client = texttospeech.TextToSpeechClient()
 
 # text input is hardcoded for now
-text_input = texttospeech.types.SynthesisInput(text="hello world")
+text_input = texttospeech.types.SynthesisInput(text="this is a test for the text to speech")
 
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
@@ -21,3 +21,9 @@ audio_config = texttospeech.types.AudioConfig(
 # text-to-speech request on the text input with the selected
 # voice parameters and audio file type
 response = client.synthesize_speech(text_input, voice, audio_config)
+
+# The response's audio_content is binary.
+with open('output.mp3', 'wb') as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
