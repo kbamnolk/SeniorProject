9cfdb416c5e7cde2b6b6d8853640ce18eb9e43a8
Edgar Berber <e_berber@u.pacific.edu>
2020-03-10 17:57:32 -0700

    merging text-text and text-speech

--- translator.py
+++ translator.py
@@ -1,13 +1,14 @@
 import argparse
 import six
+from google.cloud import texttospeech
+from google.cloud import translate_v2 as translate
 
-def translate_text():
+def text_to_text():
     """Translates text into the target language.
     Target must be an ISO 639-1 language code.
     """
     target = 'en'
     text = 'Como te llamas?'
-    from google.cloud import translate_v2 as translate
     translate_client = translate.Client()
 
     if isinstance(text, six.binary_type):
@@ -23,5 +24,34 @@ def translate_text():
     print(u'Detected source language: {}'.format(
         result['detectedSourceLanguage']))
     return result['translatedText']
+    
+def text_to_speech(input_text):
+	# Instantiates a client
+	client = texttospeech.TextToSpeechClient()
 
-translated_text = translate_text()
+	# text input is hardcoded for now
+	text_input = texttospeech.types.SynthesisInput(text=input_text)
+
+	# Build the voice request, select the language code ("en-US") and the ssml
+	# voice gender ("neutral")
+	voice = texttospeech.types.VoiceSelectionParams(
+	    language_code='en-US',
+	    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+	# Select the type of audio file you want returned
+	# audio file type is temporary
+	audio_config = texttospeech.types.AudioConfig(
+	    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+	# text-to-speech request on the text input with the selected
+	# voice parameters and audio file type
+	response = client.synthesize_speech(text_input, voice, audio_config)
+
+	# The response's audio_content is binary.
+	with open('output.mp3', 'wb') as out:
+	    # Write the response to the output file.
+	    out.write(response.audio_content)
+	    print('Audio content written to file "output.mp3"')
+
+translated_text = text_to_text()
+text_to_speech(translated_text)
