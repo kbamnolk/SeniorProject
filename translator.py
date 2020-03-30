19573a92b81a142a7958b354e3c6adf8186daf9e
Edgar Berber <e_berber@u.pacific.edu>
2020-03-29 18:32:44 -0700

    added speech-to-text into our main translator code

--- translator.py
+++ translator.py
@@ -2,6 +2,28 @@ import argparse
 import six
 from google.cloud import texttospeech
 from google.cloud import translate_v2 as translate
+#!/usr/bin/env python3
+# Requires PyAudio and PySpeech.
+#import speech_recognition as sr
+
+def speech_to_text():
+	# Record Audio
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("Say something!")
+		audio = r.listen(source)
+
+	# Speech recognition using Google Speech Recognition
+	try:
+		# for testing purposes, we're just using the default API key
+		# to use another API key, use `r.recognize_google(audio, 		key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+		# instead of `r.recognize_google(audio)`
+		print("You said: " + r.recognize_google(audio))
+	except sr.UnknownValueError:
+		print("Google Speech Recognition could not understand audio")
+	except sr.RequestError as e:
+		print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
 
 def text_to_text():
     """Translates text into the target language.
