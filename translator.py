bae7bb48d010807a38c1c46b94baec578a958599
Edgar Berber <e_berber@u.pacific.edu>
2020-04-30 19:55:19 -0700

    Completed program

--- translator.py
+++ translator.py
@@ -1,38 +1,191 @@
+
+from __future__ import division
+
+import re
+import sys
+import os
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+import pyaudio
+from six.moves import queue
 import argparse
 import six
 from google.cloud import texttospeech
 from google.cloud import translate_v2 as translate
 import pygame
 
+
+
 #!/usr/bin/env python3
 # Requires PyAudio and PySpeech.
 #import speech_recognition as sr
 
-def speech_to_text():
-	# Record Audio
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("Say something!")
-		audio = r.listen(source)
 
-	# Speech recognition using Google Speech Recognition
-	try:
-		# for testing purposes, we're just using the default API key
-		# to use another API key, use `r.recognize_google(audio, 		key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-		# instead of `r.recognize_google(audio)`
-		print("You said: " + r.recognize_google(audio))
-	except sr.UnknownValueError:
-		print("Google Speech Recognition could not understand audio")
-	except sr.RequestError as e:
-		print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Desktop/Senior Project/custom-blade-275823-fd89cb417ac7.json"
+class MicrophoneStream(object):
+
+    """Opens a recording stream as a generator yielding the audio chunks."""
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b''.join(data)
 
 
-def text_to_text():
+def listen_print_loop(responses):
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        transcript = result.alternatives[0].transcript
+
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + '\r')
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+
+        else:
+            print(transcript + overwrite_chars)
+            translated_text = text_to_text(transcript)
+            text_to_speech(translated_text)
+            
+
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            if re.search(r'\b(exit|quit)\b', transcript, re.I):
+                print('Exiting..')
+                exit()
+                break
+
+            num_chars_printed = 0
+            play_audio()
+
+
+def main():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    language_code = 'en-US'  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code)
+    streaming_config = types.StreamingRecognitionConfig(
+        config=config,
+        interim_results=True)
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (types.StreamingRecognizeRequest(audio_content=content)
+                    for content in audio_generator)
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        listen_print_loop(responses)
+
+
+def text_to_text(text):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Desktop/Senior Project/TranslatorPIKey.json"
     """Translates text into the target language.
     Target must be an ISO 639-1 language code.
     """
     target = 'en'
-    text = 'Como te llamas?'
     translate_client = translate.Client()
 
     if isinstance(text, six.binary_type):
@@ -50,32 +203,33 @@ def text_to_text():
     return result['translatedText']
     
 def text_to_speech(input_text):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Desktop/Senior Project/textToSpeechKey.json"
 	# Instantiates a client
-	client = texttospeech.TextToSpeechClient()
+    client = texttospeech.TextToSpeechClient()
 
 	# text input is hardcoded for now
-	text_input = texttospeech.types.SynthesisInput(text=input_text)
+    text_input = texttospeech.types.SynthesisInput(text=input_text)
 
 	# Build the voice request, select the language code ("en-US") and the ssml
 	# voice gender ("neutral")
-	voice = texttospeech.types.VoiceSelectionParams(
+    voice = texttospeech.types.VoiceSelectionParams(
 	    language_code='en-US',
 	    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
 	# Select the type of audio file you want returned
 	# audio file type is temporary
-	audio_config = texttospeech.types.AudioConfig(
+    audio_config = texttospeech.types.AudioConfig(
 	    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
 	# text-to-speech request on the text input with the selected
 	# voice parameters and audio file type
-	response = client.synthesize_speech(text_input, voice, audio_config)
+    response = client.synthesize_speech(text_input, voice, audio_config)
 
 	# The response's audio_content is binary.
-	with open('output.mp3', 'wb') as out:
+    with open('output.mp3', 'wb') as out:
 	    # Write the response to the output file.
 	    out.write(response.audio_content)
-	    print('Audio content written to file "output.mp3"')
+    print('Audio content written to file "output.mp3"')
 
 def play_audio():
 	pygame.mixer.init()
@@ -83,6 +237,10 @@ def play_audio():
 	pygame.mixer.music.play()
 	while pygame.mixer.music.get_busy() == True:
 		continue
+		
+
+if __name__ == '__main__':
+    main()
 translated_text = text_to_text()
 text_to_speech(translated_text)
 play_audio()
